b5e4a6ca96a475f7045342998ea3e86626a1a955
RbCaViTerminal <robert@robertvail.info>
2024-11-23 09:32:35 -0500

    sdl2 doesn't allow version 330 core :(

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -1,13 +1,12 @@
-#version 330 core
-out vec4 FragColor;
+#version 130
 
-in vec3 ourColor;
-in vec2 TexCoord;
+varying vec3 ourColor;
+varying vec2 TexCoord;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    gl_FragColor = mix(texture2D(texture1, TexCoord), texture2D(texture2, TexCoord), 0.2).abgr;
 }
--- shaders/shader.vs
+++ shaders/shader.vs
@@ -1,8 +1,8 @@
-#version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoord;
+#version 130
+attribute vec3 aPos;
+attribute vec2 aTexCoord;
 
-out vec2 TexCoord;
+varying vec2 TexCoord;
 
 uniform mat4 view;
 uniform mat4 model;
